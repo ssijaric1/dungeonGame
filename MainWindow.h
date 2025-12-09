d2c82e6bbbfdd88e4b499fe503f47d5acfb6edea
farahpiralic <115026248+farahpiralic@users.noreply.github.com>
2025-12-09 22:09:34 +0100

    Only dungeon1

---
+++ MainWindow.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <gui/Window.h>
+#include "MainView.h"
+
+class MainWindow : public gui::Window
+{
+protected:
+    MainView _mainView;
+
+public:
+    MainWindow()
+        : gui::Window(gui::Size(800, 600))
+    {
+        setTitle("Dungeon Pathfinder");
+        setCentralView(&_mainView);
+    }
+    
+    void onInitialAppearance() override
+    {
+        _mainView.setFocus();
+    }
+};
